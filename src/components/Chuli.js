87092cc5f489f7dc49338ca760fcfe17ac8dd428
IwisIcould <1563417899@qq.com>
2019-10-02 14:54:48 +0800

    为了按需加载

--- src/components/Chuli.js
+++ src/components/Chuli.js
@@ -14,11 +14,12 @@ export default class chuli extends Component {
 
     // 定义的组件   toLocaleTimeString转为一个具体的时间
     render() {
+        let canshu="a1";
         return (
             <div>
              {/* <Rightinfo></Rightinfo> */}
               <h2>我是处理组件的内容</h2>
-                <NavLink to="/chuli/leftnew">leftnew</NavLink> &ensp;
+                <NavLink to={`/chuli/leftnew${canshu}`}>leftnew</NavLink> &ensp;
                 <NavLink to="/chuli/rightinfo">rightinfo</NavLink>
                      {/* <MyLink to="/chuli/rightinfo">rightinfo</MyLink> */}
                      {/* <MyLink></MyLink> */}
@@ -27,7 +28,7 @@ export default class chuli extends Component {
                 
 
                 <Switch>
-                    <Route path="/chuli/leftnew" component={Leftnew}></Route>
+                    <Route path="/chuli/leftnew:mid" component={Leftnew}></Route>
                     <Route path="/chuli/rightinfo" component={Rightinfo}></Route>
                 </Switch>
 
